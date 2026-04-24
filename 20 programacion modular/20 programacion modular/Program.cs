367b1ac558ca29902164c3470148fb87f813756b
rinher1912 <adrian.godoy@upb.edu.co>
2026-04-24 13:48:00 -0500

    Add files via upload

---
+++ 20 programacion modular/20 programacion modular/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_programacion_modular
+{
+    internal class Program
+    { static int añoactual = 2026;
+        static void Main(string[] args)
+        {
+            string nombre = "";
+            string apellido = "";
+            Console.WriteLine("cual es su nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("cual es su apellido");
+            apellido = Console.ReadLine();
+            saludo(nombre, apellido);
+
+
+            static void borrapantalla()
+            {
+                Console.Clear();
+            }
+            static void saludo(string nombre, string apellido)
+            {
+                Console.WriteLine($"Bienvenido {nombre} {apellido}, a la programacion modular");
+            }
+
+            static int edadañonacimiento()
+            {
+                int añonacimiento
+            }
+
+        }
+    }
+}
---
+++ 21 modular1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_modular1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            mostrarmenu();
+            int opcion = capturaropcion();
+            realizaroperacion(opcion);
+
+        }
+        static void mostrarmenu()
+        {
+            Console.WriteLine("----------MEMU---------------------");
+            Console.WriteLine("1 suma               2 resta");
+            Console.WriteLine("3 multiplicacion     3 division");
+            Console.WriteLine("-----------------------------------");
+        }
+        static int capturaropcion()
+        {
+            Console.WriteLine("ingrese el numero de la operacion");
+            return int .Parse(Console.ReadLine());  
+        }
+        static void borrarpantalla() 
+        {
+            Console.Clear();
+        }
+        static void realizaroperacion(int opcion) 
+        {
+            switch (opcion) 
+            {
+                case 1:
+                    suma();
+                    break;
+                case 2:
+                    resta();
+                    break;
+                case 3:
+                    multiplicacion();
+                    break;
+                case 4:
+                    Division();
+                    break;
+            }
+        }
+        static void Division()
+        { Console.WriteLine("usted va a dividir"); }
+        static void multiplicacion()
+        { Console.WriteLine("usted va a multiplicar"); }
+        static void suma()
+        { Console.WriteLine("usted va a sumar"); }
+        static void resta()
+        { Console.WriteLine("usted va a resta"); }
+
+    }
+}
